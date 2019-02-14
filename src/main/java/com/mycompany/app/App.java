3693820a22660c7e02cf1f9ea70a3ed58ddbcd6e
muk00999esh <39761767+muk00999esh@users.noreply.github.com>
2019-02-14 21:47:16 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World1222!";
+    private final String message = "Hello World12226789!";
 
     public App() {}
 
